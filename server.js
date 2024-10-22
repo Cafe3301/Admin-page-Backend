c8d82f790bbee7d54e7e1486e02a925bd6345a92
Eduardo Alkimim <reis6@outlook.com>
2024-10-22 13:07:24 -0300

    arrumando backend admin

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ connectDB();
 
 const app = express();
 
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
+const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : [];
 
 app.use(cors({
   origin: function(origin, callback) {
