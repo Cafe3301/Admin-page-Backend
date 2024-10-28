b51c6bf427830fcea2628532ae15827177550570
Eduardo Alkimim <reis6@outlook.com>
2024-10-28 16:50:26 -0300

    arrumando appointment

--- models/Appointment.js
+++ models/Appointment.js
@@ -5,7 +5,7 @@ const appointmentSchema = new mongoose.Schema({
     userCPF: { type: String, required: true },
     userPhone: { type: String, required: true }, 
     userEmail: { type: String, required: true }, 
-    carId: { type: mongoose.Schema.Types.ObjectId, ref: 'Car', required: true },
+    carId: { type: String, required: true }, // Alterado para String
     appointmentDate: { type: Date, required: true },
     status: { type: String, default: 'Agendado' }
 });
